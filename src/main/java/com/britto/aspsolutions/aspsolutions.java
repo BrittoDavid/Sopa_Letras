6995b2765b84f9b4bc42e365391586b6ebc6fee7
David Britto <brittoargote789@gmail.com>
2022-06-25 12:42:42 -0500

    proyect to join to aspsolutions

---
+++ src/main/java/com/britto/aspsolutions/aspsolutions.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.britto.aspsolutions;
+
+import com.britto.aspsolutions.Interfaces.interfaces;
+
+/**
+ *
+ * @author britt
+ */
+public class aspsolutions {
+    
+    public static void main(String[] args) {
+        new interfaces().setVisible(true);
+    }
+}
